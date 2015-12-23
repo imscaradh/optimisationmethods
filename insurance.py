9bb14842c964c96fedc378d88260c0beefc7699f
Pascal Zingg <pzingg@fastforward.ch>
2015-12-23 17:16:14 +0100

    cleanup code

--- insurance.py
+++ insurance.py
@@ -14,7 +14,7 @@ colors = ['r', 'b', 'g']
 def main():
     rates = [50, 80, 100, 120, 150, 200, 300]
     customers = [21500, 14500, 11500, 9000, 6100, 3300, 940]
-    fig = plt.figure(1, figsize=(16,9))
+    fig = plt.figure(1, figsize=(16, 9))
     ax = fig.add_subplot(111)
     fig2 = plt.figure(2)
     bx = fig2.add_subplot(111)
@@ -35,9 +35,9 @@ def main():
         ax.plot(range(103, 300), [profit_for_rate(value) for value in range(103, 300)], colors[index])
         bx.plot(range(103, 300), [customer_by_rate(value) for value in range(103, 300)], colors[index])
         best = neldermead(150, 300)
-        print best
-        ax.plot([best[0], best[0]], [0, best[1]], colors[index]+ ':')
-        ax.plot([0, best[0]], [best[1], best[1]], colors[index]+ ':')
+        print(best)
+        ax.plot([best[0], best[0]], [0, best[1]], colors[index] + ':')
+        ax.plot([0, best[0]], [best[1], best[1]], colors[index] + ':')
 
     plt.figure(1)
     plt.savefig("profit.png")
@@ -46,7 +46,7 @@ def main():
 
 
 def profit_for_rate(rate):
-    customers = cust_func(rate)
+    customers = cust_func
     if customers:
         income = customers * rate
         damage = average_damage * customers
