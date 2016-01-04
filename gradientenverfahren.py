984a35901bfd3c931138a3a1f42fa67c39c1e344
Pascal Zingg <pzingg@fastforward.ch>
2016-01-04 16:00:18 +0100

    doc

--- gradientenverfahren.py
+++ gradientenverfahren.py
@@ -8,9 +8,9 @@ def algorithm(lmbda):
     fhelper.func = "x**2"  # raw_input("Enter function: ")
 
     x_range = np.arange(-50, 50)
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111)
-    # ax.plot(x_range, [fhelper.f(value) for value in x_range])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(x_range, [fhelper.f(value) for value in x_range])
 
     point = -5
     df_result = fhelper.df(point)
@@ -18,34 +18,35 @@ def algorithm(lmbda):
         df_result = fhelper.df(point)
         point -= lmbda * df_result
 
-    print("Precision = %f" % point)
+    print("Extremalstelle bei x = %f" % point)
 
-    # point1 = -40
-    # x_range_tan = np.arange(point1 - 20, point1 + 20)
-    # ax.plot(x_range_tan, fhelper.tan(x_range_tan, point1))
-    #
-    # point2 = -20
-    # x_range_tan = np.arange(point2 - 20, point2 + 20)
-    # ax.plot(x_range_tan, fhelper.tan(x_range_tan, point2))
-    #
-    # x_range_tan = np.arange(point - 20, point + 20)
-    # ax.plot(x_range_tan, fhelper.tan(x_range_tan, point))
-    #
-    # plt.grid(True)
-    # ax.set_xlabel("x")
-    # ax.set_ylabel("y")
-    # plt.savefig("doc/gradient.png")
+    point1 = -40
+    x_range_tan = np.arange(point1 - 20, point1 + 20)
+    ax.plot(x_range_tan, fhelper.tan(x_range_tan, point1))
+
+    point2 = -20
+    x_range_tan = np.arange(point2 - 20, point2 + 20)
+    ax.plot(x_range_tan, fhelper.tan(x_range_tan, point2))
+
+    x_range_tan = np.arange(point - 20, point + 20)
+    ax.plot(x_range_tan, fhelper.tan(x_range_tan, point))
+
+    plt.grid(True)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    plt.savefig("doc/gradient.png")
 
 
 def main():
-    values = np.arange(0.0001, 2, 0.05)
-    for lmdba in values:
-        print("Run with lambda = %s --------------------------------" % lmdba)
-        t1 = time.time()
-        algorithm(lmdba)
-        t2 = time.time()
-        delta = t2 - t1
-        print("Took %f ms \n" % delta)
+    algorithm(0.001)
+    # values = np.arange(0.0001, 2, 0.05)
+    # for lmdba in values:
+    # print("Run with lambda = %s --------------------------------" % lmdba)
+    # t1 = time.time()
+    # algorithm(lmdba)
+    # t2 = time.time()
+    # delta = t2 - t1
+    # print("Took %f ms \n" % delta)
 
 
 if __name__ == '__main__':
--- lifeguard.py
+++ lifeguard.py
@@ -8,10 +8,10 @@ global ax
 def main():
     fhelper.func = "math.sqrt(100**2+(2*100-x)**2)/2.1+math.sqrt(100**2+x**2)/4.1"
 
-    x_range = np.arange(-200, 200)
+    x_range = np.arange(-150, 400)
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.plot(x_range, [fhelper.df(value) for value in x_range])
+    ax.plot(x_range, [fhelper.f(value) for value in x_range])
 
     plt.grid(True)
     ax.set_xlabel("Way [x]")
